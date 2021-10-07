b855f289f8df2406a775c60787858a75dde5ec1a
Sahil Raheja <mailbox2sahil@gmail.com>
2021-10-08 00:52:28 +0530

    Api url change

--- src/constants/apiConstants.jsx
+++ src/constants/apiConstants.jsx
@@ -1,6 +1,6 @@
 export const GlobalConstants = {
-    API_BASE_URL: "https://localhost:44375/api/POS",
-    //API_BASE_URL: "http://164.52.216.130/restaurants/api/POS",
+    //API_BASE_URL: "https://localhost:44375/api/POS",
+    API_BASE_URL: "http://164.52.216.130/restaurants/api/POS",
     ACCESS_TOKEN_NAME: 'login_access_token',
     EXPIRY_DATE_NAME: 'ExpiryDate',
     REFRESH_TOKEN_NAME: 'RefreshToken',
